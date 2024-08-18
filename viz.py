ac79d4967b27fa95de8d019243807be66722556b
KellerJordan <kellerjordan1998@gmail.com>
2024-08-18 02:00:30 +0000

    .

--- viz.py
+++ viz.py
@@ -6,6 +6,8 @@ from tqdm import tqdm
 import scipy.stats
 import airbench
 
+show_sig_only = True
+
 def margin(outs, labs):
     ind = torch.arange(len(labs), device=labs.device)
     tgts = outs[..., ind, labs]
@@ -25,26 +27,45 @@ print('Loading the outputs produced by all the models that were trained...')
 outs1 = torch.stack([torch.load(p) for p in tqdm(glob.glob('nets%d_default/*' % i))])
 outs2 = torch.stack([torch.load(p) for p in tqdm(glob.glob('nets%d_minus_n40/*' % i))])
 
-print('Computing the correct-class margins for each example which was ablated')
+print('Computing the correct-class margins for each of the %d examples which were ablated' % len(indices))
 mm1 = margin(outs1, train_loader.labels).float().cpu()
 mm2 = margin(outs2, train_loader.labels).float().cpu()
 diff = (mm1.mean(0) - mm2.mean(0))
 std = (mm1.var(0) / len(mm1) + mm2.var(0) / len(mm2))**0.5
 zz = (diff / std) # z-score of each difference
 
-print('Example index\t   margin  \t\tself-influence\t\t\tp-value')
+print()
+if show_sig_only:
+    print('Showing examples whose estimated self-influence is statistically-significantly different from zero (p < 0.01):')
+    print()
+
+if show_sig_only:
+    print('Example index\t   margin  \t\tself-influence\t\t\tp-value')
+else:
+    print('Example index\t   margin  \t\tself-influence\t\t\tp-value\tsignificant? (p < 0.01)')
 print('\t\twith\twithout')
 print()
 print('Random examples:')
+res = []
 for j in range(40):
     i = indices[j]
     z_score = zz[i].item()
     p_value = scipy.stats.norm.sf(abs(z_score)) * 2
-    print('%d\t\t%.3f\t%.3f\t\t%+.3f\t\t\t\t%.4f' % (i, mm2.mean(0)[i], mm1.mean(0)[i], diff[i], p_value))
+    sig = (p_value < 0.01)
+    if show_sig_only:
+        if sig:
+            print('%d\t\t%.3f\t%.3f\t\t%+.3f\t\t\t\t%.4f' % (i, mm2.mean(0)[i], mm1.mean(0)[i], diff[i], p_value))
+        res.append(diff[i].item())
+    else:
+        print('%d\t\t%.3f\t%.3f\t\t%+.3f\t\t\t\t%.4f\t%s' % (i, mm2.mean(0)[i], mm1.mean(0)[i], diff[i], p_value,
+              'yes' if sig else 'no'))
+        res.append(diff[i].item())
     if j == 19:
-        print('Average:\t\t\t\t%+.3f' % diff[indices[:20]].mean())
+        print('Average:\t\t\t\t%+.3f' % torch.tensor(res).mean())
+        res = []
         print()
         print('Easy examples:')
     if j == 39:
-        print('Average:\t\t\t\t%+.3f' % diff[indices[20:]].mean())
+        print('Average:\t\t\t\t%+.3f' % torch.tensor(res).mean())
+        pass
 
